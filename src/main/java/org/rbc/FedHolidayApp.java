05d4e0281e950d2f3a25fa50ad8a023ec4f6a447
souvikoffice1991 <souvikoffice1991@gmail.com>
2026-04-01 02:10:01 -0400

    checkin swagger implementation

--- src/main/java/org/rbc/FedHolidayApp.java
+++ src/main/java/org/rbc/FedHolidayApp.java
@@ -3,6 +3,7 @@ package org.rbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FedHolidayApp {
 
--- src/main/java/org/rbc/controller/HolidayController.java
+++ src/main/java/org/rbc/controller/HolidayController.java
@@ -1,5 +1,9 @@
 package org.rbc.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name = "Holiday", description = "the Holiday Api")
 @RequestMapping("/api/fedhol")
 public class HolidayController {
 
@@ -22,11 +27,23 @@ public class HolidayController {
     }
 
 
+    @Operation(
+            summary = "Creates a new holiday",
+            description = "Creates a new holiday for a country, if no country is provided creates holiday for every country")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
     @PostMapping(value = "/add-Holiday")
     public ResponseEntity<HolidayResponse> create(@Valid @RequestBody HolidayRequest holidayRequest) {
        return new ResponseEntity<>(holidayService.addOrUpdateHoliday(holidayRequest,Boolean.FALSE),HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Gets list of holiday for a country",
+            description = "Sends list of holiday for the input country")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
     @GetMapping(value="/country-list/{name}")
     public ResponseEntity<List<HolidayResponse>> getListOfFederalHolidays(@PathVariable @Pattern(regexp = "^[A-Za-z]+$", message = "Only alphabets allowed") @NotNull String name) {
         return new ResponseEntity<>(holidayService.getHolidays(name), HttpStatus.OK);
